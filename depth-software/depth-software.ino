0876d7b958080db429064f1477f6741e49fdd1e4
Dexter Fichuk <Dexter Fichuk>
2016-11-15 16:26:01 -0500

    Implented ethercard and post without testing

--- depth-software/depth-software.ino
+++ depth-software/depth-software.ino
@@ -20,6 +20,24 @@ dht DHT;
 
 #define DHT11_PIN 2 //Air sensor pin
 
+//ETHERNET STUFF
+// Enter a MAC address for your controller below.
+// Newer Ethernet shields have a MAC address printed on a sticker on the shield
+byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+// if you don't want to use DNS (and reduce your sketch size)
+// use the numeric IP instead of the name for the server:
+IPAddress server(159,203,4,227);  // numeric IP for Google (no DNS)
+//char server[] = "www.google.com";    // name address for Google (using DNS)
+
+// Set the static IP address to use if the DHCP fails to assign
+IPAddress ip(192, 168, 0, 177);
+
+// Initialize the Ethernet client library
+// with the IP address and port of the server
+// that you want to connect to (port 80 is default for HTTP):
+EthernetClient client;
+//END ETHERNET STUFF
+
 void setup(void) {
   Serial.begin(9600);
 }
@@ -173,5 +191,49 @@ void loop(void) {
 
   //Already delayed in the ph calculations
   //delay(2000);
-
+  ethernetPost();
 }
+
+void ethernetPost(){
+  // start the Ethernet connection:
+  if (Ethernet.begin(mac) == 0) {
+    Serial.println("Failed to configure Ethernet using DHCP");
+    // try to congifure using IP address instead of DHCP:
+    Ethernet.begin(mac, ip);
+  }
+  // give the Ethernet shield a second to initialize:
+  delay(1000);
+  Serial.println("connecting...");
+
+  // if you get a connection, report back via serial:
+  if (client.connect(server, 80)) {
+    Serial.println("connected");
+    // Make a HTTP request:
+    client.println("GET /depth/add.php?key=Saucy&nodeid=3&waterTemp=38 HTTP/1.1");
+    client.println("Host: 159.203.4.227");
+    client.println("Connection: close");
+    client.println();
+  } else {
+    // if you didn't get a connection to the server:
+    Serial.println("connection failed");
+  }
+  checkResponse();
+}
+
+void checkResponse(){
+    if (client.available()) {
+    char c = client.read();
+    Serial.print(c);
+  }
+
+  // if the server's disconnected, stop the client:
+  if (!client.connected()) {
+    Serial.println();
+    Serial.println("disconnecting.");
+    client.stop();
+
+    // do nothing forevermore:
+    while (true);
+  }
+}
+
