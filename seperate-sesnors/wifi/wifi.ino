5cea086f49ef90b6fedd5b67b338f30ff7968196
Dexter Fichuk <dexterfichuk@gmai.com>
2017-04-24 17:53:54 -0400

    Update DB

--- seperate-sesnors/wifi/wifi.ino
+++ seperate-sesnors/wifi/wifi.ino
@@ -16,12 +16,15 @@
 dht DHT;
 
 //Declare Variables 
-
+String const NODENUM = "5005";
 float offset = 0.0; //Calculated from measuring with chem thing
 
 int ph_pin = A0;        // pH Pin        : A0
 #define DHT11_PIN 16    // Humidity Pin  : D2
 #define ONE_WIRE_BUS 12 // Water Temp    : D8
+
+//Depth DB API
+const char* host = "http://159.203.4.227/depth/add.php?key=Saucy&";
 
 OneWire oneWire(ONE_WIRE_BUS); 
 
@@ -65,7 +68,65 @@ struct air_result {
   } values;
  
 void loop() {
-    // put your main code here, to run repeatedly:
+  waterTemp();
+  air();
+  pH(); 
+
+  Serial.print("Water Temperature = ");
+  Serial.print(values.waterTemp);
+  Serial.print(" Celsius");
+  Serial.println();
+  
+  Serial.print("Air Temperature = ");
+  Serial.print(values.airTemp);
+  Serial.print(" Celsius");
+  Serial.println();
+  
+  Serial.print("Air Humidity = ");
+  Serial.print(values.humid);
+  Serial.println();
+
+  Serial.print("pH = ");
+  Serial.print(values.pH);
+  Serial.println();
+
+  delay(1000);
+}
+
+void updateDB(){
+  WiFiClient client;
+  const int httpPort = 80;
+  if (!client.connect(host, httpPort)) {
+    Serial.println("connection failed");
+    return;
+  }
+    // We now create a URI for the request
+  String url = "nodeID=";
+  url += NODENUM;
+  url += "&airTemp=";
+  url += values.airTemp;
+  url += "&airHumidity=";
+  url += values.humid;
+  url += "&pH=";
+  url += values.pH;
+  url += "&waterTemp=";
+  url += values.waterTemp;
+  
+  Serial.print("Requesting URL: ");
+  Serial.println(url);
+  
+  // This will send the request to the server
+  client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+               "Host: " + host + "\r\n" + 
+               "Connection: close\r\n\r\n");
+  unsigned long timeout = millis();
+  while (client.available() == 0) {
+    if (millis() - timeout > 5000) {
+      Serial.println(">>> Client Timeout !");
+      client.stop();
+      return;
+   }
+  }
 }
 
 void waterTemp(){
